4af256066587f8b59fb88b93d1c0d3e309ad9324
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2026-09-20 10:09:21 -0300

    Ajusta espera dos elementos de login no BrowserStack

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,36 +1,40 @@
-import { $, driver } from '@wdio/globals'
+import { $, driver } from "@wdio/globals";
 
 class LoginPage {
+  get email() {
+    return $("id:email");
+  }
 
-    get email() {
-        return $('id:email')
+  get password() {
+    if (driver.isAndroid) {
+      return $('android=new UiSelector().text("Password")');
     }
 
-    get password() {
-        if (driver.isAndroid) {
-            return $('android=new UiSelector().text("Password")')
-        }
+    if (driver.isIOS) {
+      return $('-ios predicate string:name == "Password"');
+    }
+  }
 
-        if (driver.isIOS) {
-            return $('-ios predicate string:name == "Password"')
-        }
+  get btnLogin() {
+    if (driver.isAndroid) {
+      return $("~Login");
     }
 
-    get btnLogin() {
-        if (driver.isAndroid) {
-            return $('~Login')
-        }
-
-        if (driver.isIOS) {
-            return $('~btnLogin')
-        }
+    if (driver.isIOS) {
+      return $("~btnLogin");
     }
+  }
 
-    async login(email, password) {
-        await this.email.setValue(email)
-        await this.password.setValue(password)
-        await this.btnLogin.click()
-    }
+  async login(email, password) {
+    await this.email.waitForDisplayed({ timeout: 20000 });
+    await this.email.setValue(email);
+
+    await this.password.waitForDisplayed({ timeout: 20000 });
+    await this.password.setValue(password);
+
+    await this.btnLogin.waitForDisplayed({ timeout: 20000 });
+    await this.btnLogin.click();
+  }
 }
 
-export default new LoginPage()
+export default new LoginPage();
